dcb89724a6d2cac1ea2cf23b54111079f9594371
Lorenzo123herobrine <159460377+Lorenzo123herobrine@users.noreply.github.com>
2025-02-14 12:16:25 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,30 @@
+print("App Comido!\n")
+
+print("1. Cadastras lancheria ")
+
+print("2. Listar lancheria")
+
+print("3. Ativar lancheria")
+
+print("4. Sair")
+
+opcao_escolhida = int (input("Escolha sua escolha: \n " ))
+
+print (f"Você escolheu a opção {opcao_escolhida}") 
+
+if opcao_escolhida == 1: 
+    
+    print ("Cadastras lancheria")
+
+elif opcao_escolhida == 2:
+    
+    print("Listar lancheria")
+
+elif opcao_escolhida == 3:
+    
+    print("Ativar lancheria")
+
+else:
+    
+    print("Sair")
+
