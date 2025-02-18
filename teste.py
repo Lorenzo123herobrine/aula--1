58c4f3c8cb8f216bbd337ac47b7d1605f536fe5c
Lorenzo123herobrine <159460377+Lorenzo123herobrine@users.noreply.github.com>
2025-02-18 09:57:04 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,28 +1,35 @@
-print("App Comido!\n")
+import os
 
-print("1. Cadastras lancheria ")
+def exibir_nome_do_programa():
+    print("App Comido!\n")
 
-print("2. Listar lancheria")
-
-print("3. Ativar lancheria")
-
-print("4. Sair")
+def exibir_opcoes():
+    print("1. Cadastras lancheria ")
+    print("2. Listar lancheria")
+    print("3. Ativar lancheria")
+    print("4. Sair")
 
 opcao_escolhida = int (input("Escolha sua escolha: \n " ))
 
 print (f"Você escolheu a opção {opcao_escolhida}") 
 
 def finalizar_app():
+    os.system("cls")
     print("Finalizar o app")
 
-if opcao_escolhida == 1: 
-    print ("Cadastras lancheria")
+    if opcao_escolhida == 1: 
+        print ("Cadastras lancheria")
+    elif opcao_escolhida == 2:
+        print("Listar lancheria")
+    elif opcao_escolhida == 3:
+        print("Ativar lancheria")
+    else:
+        finalizar_app()
 
-elif opcao_escolhida == 2:
-    print("Listar lancheria")
 
-elif opcao_escolhida == 3:
-    print("Ativar lancheria")
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
 
-else:
-    finalizar_app()
+if __name__=='__main__':
+
